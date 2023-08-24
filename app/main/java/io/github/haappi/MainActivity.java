1885ac6fae1cde10c8cc537029c97f0ea2ed3441
haappi <74637670+haappi@users.noreply.github.com>
2023-08-24 14:33:08 -0500

    e

--- app/main/java/io/github/haappi/MainActivity.java
+++ app/main/java/io/github/haappi/MainActivity.java
@@ -1,16 +1,14 @@
-package com.example.myapplication;
-
-import android.os.Bundle;
+package io.github.haappi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondScreen extends AppCompatActivity {
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_screen);
-
-
+        setContentView(R.layout.activity_main);
     }
 }
