1b4b54897b0a50a7019058894490deac643974cd
haappi <74637670+haappi@users.noreply.github.com>
2023-08-25 11:44:55 -0500

    e?

--- app/src/androidTest/java/io/github/haappi/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/github/haappi/ExampleInstrumentedTest.java
@@ -1,14 +1,14 @@
-package com.example.myapplication;
-
-import static org.junit.Assert.*;
+package io.github.haappi;
 
 import android.content.Context;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+        assertEquals("io.github.haappi", appContext.getPackageName());
     }
 }
--- app/src/test/java/io/github/haappi/ExampleUnitTest.java
+++ app/src/test/java/io/github/haappi/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.example.myapplication;
-
-import static org.junit.Assert.*;
+package io.github.haappi;
 
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
