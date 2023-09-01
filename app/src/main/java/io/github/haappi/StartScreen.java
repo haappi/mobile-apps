aa0bda2e8cd525dbe17f1ff2284551c453d860c9
haappi <74637670+haappi@users.noreply.github.com>
2023-09-01 14:28:22 -0500

    commit just in case

--- app/src/main/java/io/github/haappi/StartScreen.java
+++ app/src/main/java/io/github/haappi/StartScreen.java
@@ -1,9 +1,15 @@
 package io.github.haappi;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,7 +22,8 @@ import io.github.haappi.databinding.ActivityMainBinding;
 public class StartScreen extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private ActivityMainBinding binding;    private GestureDetector gestureDetector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,10 @@ public class StartScreen extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController =
-                Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavController navController =
+//                Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         binding.fab.setOnClickListener(
                 new View.OnClickListener() {
@@ -42,6 +49,53 @@ public class StartScreen extends AppCompatActivity {
                                 .show();
                     }
                 });
-        findViewById(R.id.play_button).setOnClickListener(event -> {});
+//        AppCompatButton playButton = findViewById(R.id.play_button);
+//        playButton.setOnClickListener(event -> {
+//            Log.d("button", "play button pressed");
+//        });
+
+
+        RelativeLayout rectangularBox = findViewById(R.id.rectangularBox);
+        gestureDetector = new GestureDetector(this, new GestureHandler());
+
+        rectangularBox.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetector.onTouchEvent(event);
+                return true;
+            }
+        });
     }
+
+    private final class GestureHandler extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            float deltaX = e2.getX() - e1.getX();
+            float deltaY = e2.getY() - e1.getY();
+
+            if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                // Horizontal swipe
+                if (deltaX > 0) {
+                    // Right to left swipe
+                    Toast.makeText(StartScreen.this, "Right to Left Swipe", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Left to right swipe
+                    Toast.makeText(StartScreen.this, "Left to Right Swipe", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                // Vertical swipe
+                if (deltaY > 0) {
+                    // Down to up swipe
+                    Toast.makeText(StartScreen.this, "Down to Up Swipe", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Up to down swipe
+                    Toast.makeText(StartScreen.this, "Up to Down Swipe", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            return true;
+        }
+
+    }
+
 }
